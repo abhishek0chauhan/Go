c08a9c4968bb2db650ea1f552970d1702390e271
abhishek0chauhan <abhi45chauhan@gmail.com>
2023-09-04 02:04:20 +0530

    mongodb golang API

--- 24buildapi/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/abhishek0chauhan/buildapi
-
-go 1.20
-
-require github.com/gorilla/mux v1.8.0 // indirect
---
+++ 25mongoAPI/controller/controller.go
@@ -0,0 +1,137 @@
+package controller
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"command-line-argumentsC:\\Users\\Abhishek\\OneDrive\\Desktop\\my-work\\go\\Go\\25mongoAPI\\model\\models.go"
+	model "command-line-argumentsC:\\Users\\Abhishek\\OneDrive\\Desktop\\my-work\\go\\Go\\25mongoAPI\\model\\models.go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
+)
+
+const connectionString = "mongodb+srv://abhishek:<Abhishek>@cluster0.rjj1641.mongodb.net/?retryWrites=true&w=majority"
+const dbName = "netflix"
+const colName = "watchlist"
+
+// Most important
+var collection *mongo.Collection
+
+// connect with mongoDB
+func init() {
+	// client options
+	clientOptions := options.Client().APPlyURI(connectionString)
+
+	//connect to mongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("MongoDB connection success")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	// collection instance
+	fmt.Println("Collection instance is ready")
+}
+
+// MongoDB hepers - file
+
+// inser 1 records
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InserOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inseretd one movie in db with id: ", inserted.InsertedID)
+}
+
+// update one movie
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count: ", result.ModifiedCount)
+}
+
+// delete  movie
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("movie got deleted with delete count: ", deleteCount)
+}
+
+// delete all records from mongoDB
+func deleteAllMovie() int64 {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Number of movies deleted: ", deleteResult.DeleteCount)
+	return deleteResult.DeleteCount
+}
+
+// get all movies
+func getAllMovies() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var movies []primitive.M
+
+	for cur.Next(context.Background()) {
+		var movie bson.M
+		err := cur.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+
+	defer cur.Close(context.Background())
+	return movies
+}
+
+// Actual controller - file
+func GetMyAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-ww-form-urlencode")
+	allMovies := getAllMovies()
+	json.NewEncoder(w).Encode(allMovies)
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-ww-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	var movie model.Netflix
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	insertOneMovie(movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func MarkAsWatched(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-ww-form-urlencode")
+
+}
